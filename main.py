63a5219598f35a98904213bb03604c5f6185b02c
Le Hoang Kim Thanh <81308733+Kimthanh11@users.noreply.github.com>
2023-05-16 13:38:42 +0700

    Update task1,task2

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 from task1 import *
 from task2 import *
-scheduler = 
+from scheduler import *
+
+schedule = Scheduler()
+Scheduler.SCH_Init()
+
+task1 = Task1()
+task2 = Task2()
+
+task1.Task1_Run()
+task2.Task2_Run()

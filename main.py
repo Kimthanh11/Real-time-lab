70635956a2d166f74f41286be931ed9d4af76b24
Le Hoang Kim Thanh <81308733+Kimthanh11@users.noreply.github.com>
2023-05-16 14:50:03 +0700

    Update course

--- main.py
+++ main.py
@@ -1,36 +1,36 @@
-# from task1 import *
-# from task2 import *
-# from scheduler import *
-
-# schedule = Scheduler()
-# schedule.SCH_Init()
-
-# task1 = Task1()
-# task2 = Task2()
-
-# schedule.SCH_Add_Task(task1.Task1_Run, 1000, 50000)
-# schedule.SCH_Add_Task(task2.Task2_Run, 2000, 60000)
-
-
-# while True:
-#     schedule.SCH_Update()
-#     schedule.SCH_Dispatch_Tasks()
-
-print("Hello LAB2")
-import time
-from scheduler import *
 from task1 import *
 from task2 import *
-scheduler = Scheduler()
-scheduler.SCH_Init()
+from scheduler import *
+
+schedule = Scheduler()
+schedule.SCH_Init()
 
 task1 = Task1()
 task2 = Task2()
 
-scheduler.SCH_Add_Task(task1.Task1_Run, 1000,2000)
-scheduler.SCH_Add_Task(task2.Task2_Run, 2000,4000)
+schedule.SCH_Add_Task(task1.Task1_Run, 1000, 50000)
+schedule.SCH_Add_Task(task2.Task2_Run, 2000, 60000)
+
 
 while True:
-    scheduler.SCH_Update()
-    scheduler.SCH_Dispatch_Tasks()
-    time.sleep(1)
+    schedule.SCH_Update()
+    schedule.SCH_Dispatch_Tasks()
+
+# print("Hello LAB2")
+# import time
+# from scheduler import *
+# from task1 import *
+# from task2 import *
+# scheduler = Scheduler()
+# scheduler.SCH_Init()
+
+# task1 = Task1()
+# task2 = Task2()
+
+# scheduler.SCH_Add_Task(task1.Task1_Run, 1000,2000)
+# scheduler.SCH_Add_Task(task2.Task2_Run, 2000,4000)
+
+# while True:
+#     scheduler.SCH_Update()
+#     scheduler.SCH_Dispatch_Tasks()
+#     time.sleep(1)

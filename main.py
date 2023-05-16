b506e6e2af5d0aca7550bd7f21cdc1755b0acbc4
Le Hoang Kim Thanh <81308733+Kimthanh11@users.noreply.github.com>
2023-05-16 13:57:32 +0700

    Update teacher's file

--- main.py
+++ main.py
@@ -1,18 +1,36 @@
+# from task1 import *
+# from task2 import *
+# from scheduler import *
+
+# schedule = Scheduler()
+# schedule.SCH_Init()
+
+# task1 = Task1()
+# task2 = Task2()
+
+# schedule.SCH_Add_Task(task1.Task1_Run, 1000, 50000)
+# schedule.SCH_Add_Task(task2.Task2_Run, 2000, 60000)
+
+
+# while True:
+#     schedule.SCH_Update()
+#     schedule.SCH_Dispatch_Tasks()
+
+print("Hello LAB2")
+import time
+from scheduler import *
 from task1 import *
 from task2 import *
-from scheduler import *
-
-schedule = Scheduler()
-schedule.SCH_Init()
+scheduler = Scheduler()
+scheduler.SCH_Init()
 
 task1 = Task1()
 task2 = Task2()
 
-schedule.SCH_Add_Task(task1.Task1_Run, 1000, 50000)
-schedule.SCH_Add_Task(task2.Task2_Run, 2000, 60000)
-
+scheduler.SCH_Add_Task(task1.Task1_Run, 1000,2000)
+scheduler.SCH_Add_Task(task2.Task2_Run, 2000,4000)
 
 while True:
-    schedule.SCH_Update()
-    schedule.SCH_Dispatch_Tasks()
-
+    scheduler.SCH_Update()
+    scheduler.SCH_Dispatch_Tasks()
+    time.sleep(1)

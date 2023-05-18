98056d60915b3c5e193aa5aa76f6ef04d9dcebf4
Le Hoang Kim Thanh <81308733+Kimthanh11@users.noreply.github.com>
2023-05-18 13:36:56 +0700

    Update socket

---
+++ getSocketInfo.java
@@ -0,0 +1,22 @@
+import java.net.Socket;
+import java.net.*;
+import java.io.*;
+
+public class getSocketInfo {
+    public static void main(String[] args) {
+        for (int i = 0; i < args.length; i++) {
+            try {
+                Socket theSocket = new Socket(args[i], 80);
+                System.out.println("Connected to " + theSocket.getInetAddress() + " on port " + theSocket.getPort()
+                        + " from port " + theSocket.getLocalPort() + " of " + theSocket.getLocalAddress());
+            } catch (UnknownHostException e) {
+                System.err.println("I can't find " + args[i]);
+            } catch (SocketException e) {
+                System.err.println("Cound not connect to " + args[i]);
+
+            } catch (IOException e) {
+                System.err.println(e);
+            }
+        }
+    }
+}
